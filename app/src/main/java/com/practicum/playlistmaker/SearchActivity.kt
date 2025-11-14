f17304f4461a5617aa80cf52dbe3f76dadfbf6e8
Denis Mikryukov <den20011709@gmail.com>
2025-11-14 13:41:10 +0500

    build search screen

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,6 +1,5 @@
 package com.practicum.playlistmaker
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -19,16 +18,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -44,6 +42,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.practicum.playlistmaker.utils.TopAppButtonBar
 
 class SearchActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,7 +60,16 @@ fun SearchScreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White),
-        topBar = { TopAppButtonBar(context) }
+        topBar = {
+            TopAppButtonBar(
+                context = context,
+                text =stringResource(R.string.search),
+                onClick = {
+                    val intent = Intent(context, MainActivity::class.java)
+                    context.startActivity(intent)
+                }
+            )
+        }
     ) { paddingValues ->
         CustomSearchField(
             modifier = Modifier
@@ -166,37 +174,87 @@ fun CustomSearchField(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun TopAppButtonBar(context: Context) {
-    TopAppBar(
-        title = {
-            Text(
-                text = stringResource(R.string.search),
-                style = MaterialTheme.typography.titleMedium,
-                fontSize = 22.sp,
-            )
-        },
-        navigationIcon = {
-            Icon(
-                modifier = Modifier
-                    .padding(horizontal = 16.dp)
-                    .size(24.dp)
-                    .clickable(
-                        onClick = {
-                            val intent = Intent(context, MainActivity::class.java)
-                            context.startActivity(intent)
-                        }
-                    ),
-                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                contentDescription = stringResource(R.string.arrow_back)
-            )
-        }
-    )
-}
-
 @Preview
 @Composable
 private fun SearchScreenPreview() {
     SearchScreen()
+}
+
+@Composable
+private fun TextFieldSearchScreen() {
+    val context = LocalContext.current
+    var searchRequest: String by remember { mutableStateOf("") }
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        topBar = {
+            TopAppButtonBar(
+                context = context,
+                text = stringResource(R.string.search),
+                onClick = {
+                    val intent = Intent(context, MainActivity::class.java)
+                    context.startActivity(intent)
+                }
+            )
+        }
+    ) { paddingValues ->
+        OutlinedTextField(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            value = searchRequest,
+            onValueChange = { searchRequest = it },
+            singleLine = true,
+            label = {
+                Text(
+                    text = stringResource(R.string.search),
+                    fontSize = 16.sp,
+                )
+            },
+            leadingIcon = {
+                Icon(
+                    modifier = Modifier
+                        .size(18.dp),
+                    imageVector = Icons.Default.Search,
+                    contentDescription = stringResource(R.string.search)
+                )
+            },
+            trailingIcon = {
+                if (!searchRequest.isEmpty()) {
+                    Icon(
+                        modifier = Modifier
+                            .size(14.dp)
+                            .clickable(
+                                onClick = { searchRequest = "" }
+                            ),
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = stringResource(R.string.search),
+                        tint = Color.Gray
+                    )
+                }
+            },
+            shape = MaterialTheme.shapes.small,
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = Color.LightGray,
+                unfocusedContainerColor = Color.LightGray,
+                focusedTextColor = Color.Black,
+                unfocusedTextColor = Color.Black,
+                focusedLabelColor = Color.Gray,
+                unfocusedLabelColor = Color.Gray,
+                focusedLeadingIconColor = Color.Gray,
+                unfocusedLeadingIconColor = Color.Gray,
+                focusedBorderColor = Color.Transparent,
+                unfocusedBorderColor = Color.Transparent
+            ),
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun TextFieldSearchScreenPreview() {
+    TextFieldSearchScreen()
 }
